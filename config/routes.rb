69c94d758d4288f1428fc6368cd82664d732cb3d
Raghu Betina <rbetina@github.com>
2015-04-20 13:43:28 -0500

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,78 +1,23 @@
 Rails.application.routes.draw do
-  get 'essentials/basics_1'
+  get("essentials/1", { :controller => "essentials", :action => "basics_1" })
 
-  get 'essentials/arrays_2'
+  get("essentials/2", { :controller => "essentials", :action => "arrays_2" })
 
-  get 'essentials/hashes_3'
+  get("essentials/3", { :controller => "essentials", :action => "hashes_3" })
 
-  get 'essentials/nested_lists_4'
+  get("essentials/4", { :controller => "essentials", :action => "nested_lists_4" })
 
-  get 'essentials/each_5'
+  get("essentials/5", { :controller => "essentials", :action => "each_5" })
 
-  get 'essentials/methods_6'
+  get("essentials/6", { :controller => "essentials", :action => "methods_6" })
 
-  get 'essentials/arguments_7'
+  get("essentials/7", { :controller => "essentials", :action => "arguments_7" })
 
-  get 'essentials/return_values_8'
+  get("essentials/8", { :controller => "essentials", :action => "return_values_8" })
 
-  get 'essentials/classes_9'
+  get("essentials/9", { :controller => "essentials", :action => "classes_9" })
 
-  get 'essentials/challenge_10'
+  get("essentials/10", { :controller => "essentials", :action => "challenge_10" })
 
-  get 'essentials/friendbc_11'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get("essentials/11", { :controller => "essentials", :action => "friendbc_11" })
 end
