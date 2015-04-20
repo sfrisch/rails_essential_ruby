04c184c9791fa4276062b4baa93c86182785042d
Raghu Betina <rbetina@github.com>
2015-04-20 13:52:53 -0500

    Adds 3

--- app/controllers/essentials_controller.rb
+++ app/controllers/essentials_controller.rb
@@ -15,6 +15,8 @@ class EssentialsController < ApplicationController
     @message1 = "Howdy, " + full + ". Your lucky number for today is " + lucky_number.to_s + "."
     # is harder than with String Interpolation
     @message2 = "Howdy, #{full}. Your lucky number for today is #{lucky_number}."
+
+    render('basics_1.html.erb')
   end
 
   def arrays_2
@@ -25,32 +27,61 @@ class EssentialsController < ApplicationController
 
     # Adding an element to an Array:
     @students.push("Roz")
+
+    render('arrays_2.html.erb')
   end
 
   def hashes_3
+    # Hashes
+
+    # Creating Hashes
+    @person1 = { "first_name" => "Raghu", "last_name" => "Betina" }
+    @person2 = { "first_name" => "Michael", "last_name" => "Arthur" }
+
+    # Adding a key/value pair to a Hash:
+    @person2["role"] = "Student"
+    @person1["role"] = "Instructor"
+
+    render('hashes_3.html.erb')
   end
 
   def nested_lists_4
+
+    render('nested_lists_4.html.erb')
   end
 
   def each_5
+
+    render('each_5.html.erb')
   end
 
   def methods_6
+
+    render('methods_6.html.erb')
   end
 
   def arguments_7
+
+    render('arguments_7.html.erb')
   end
 
   def return_values_8
+
+    render('return_values_8.html.erb')
   end
 
   def classes_9
+
+    render('classes_9.html.erb')
   end
 
   def challenge_10
+
+    render('challenge_10.html.erb')
   end
 
   def friendbc_11
+
+    render('friendbc_11.html.erb')
   end
 end
