63466399907898f3f5e69e3e066408844e3dfee1
Raghu Betina <rbetina@github.com>
2015-04-20 18:17:51 -0500

    Removes js

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get("essentials/1", { :controller => "essentials", :action => "basics_1" })
+  get("/essentials/1", { :controller => "essentials", :action => "basics_1" })
 
-  get("essentials/2", { :controller => "essentials", :action => "arrays_2" })
+  get("/essentials/2", { :controller => "essentials", :action => "arrays_2" })
 
-  get("essentials/3", { :controller => "essentials", :action => "hashes_3" })
+  get("/essentials/3", { :controller => "essentials", :action => "hashes_3" })
 
-  get("essentials/4", { :controller => "essentials", :action => "nested_lists_4" })
+  get("/essentials/4", { :controller => "essentials", :action => "nested_lists_4" })
 
-  get("essentials/5", { :controller => "essentials", :action => "each_5" })
+  get("/essentials/5", { :controller => "essentials", :action => "each_5" })
 
-  get("essentials/6", { :controller => "essentials", :action => "methods_6" })
+  get("/essentials/6", { :controller => "essentials", :action => "methods_6" })
 
-  get("essentials/11", { :controller => "essentials", :action => "friendbc_11" })
+  get("/essentials/11", { :controller => "essentials", :action => "friendbc_11" })
 end
