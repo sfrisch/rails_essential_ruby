62808f30dd9f4e2fe608ffe8eaad604bc6e34c94
sfrisch <simon.frisch@gmail.com>
2015-06-01 19:15:49 -0500

    update
    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
+config.action_mailer.default_url_options = { :host => 'www.hotelwherever.com' }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
