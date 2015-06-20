fc354bf0cb80cfd6b7937dfef83dd91d1238feaa
sfrisch <simon.frisch@gmail.com>
2015-06-20 16:02:35 -0500

    fixingbugs
    fixingbugs

--- app/controllers/essentials_controller.rb
+++ app/controllers/essentials_controller.rb
@@ -328,11 +328,11 @@ ActiveRecord::Base.connection.execute("COMMIT;")
 
 
 
-
-
 @hotelslist =@hotelslist.map(&:strip).join(', ')
 
 
+
+if @hotellist == nil then @hotelslist = '000000' end
 
 
 sql = "SELECT hotels.\"EANHotelID\",\"ChainCodeID\",airport_lat_long.\"LAT\",airport_lat_long.\"LONG\" FROM hotels INNER JOIN airport_lat_long ON airport_lat_long.\"AIRPORT\" = hotels.\"AirportCode\" INNER JOIN hoteltemp ON hotels.\"EANHotelID\" = hoteltemp.\"eanhotelid\" WHERE hoteltemp.\"eanhotelid\" IN (#{@hotelslist}) "
